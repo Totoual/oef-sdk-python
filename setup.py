0caeb34fd38711781dfc4bc621930fcd66fe71ae
Marco Favorito <marco.favorito@gmail.com>
2018-12-10 18:33:05 +0000

    Fix setup.py removing os-dependent hard-coded path separators.

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # Copyright (C) Fetch.ai 2018 - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
-# Written by Tom Nicholson <tom.nicholson@fetch.ai>
 import distutils.cmd
 import distutils.log
 import fileinput
 import os
 import re
+import shutil
 import subprocess
 import glob
 
 import setuptools.command.build_py
 from setuptools import setup
 
-#TODO support installation for Windows
 
 class ProtocCommand(distutils.cmd.Command):
     """A custom command to generate Python Protobuf modules from OEFCoreProtocol"""
 
-    description = "Generate Python Protobuf modules from OEFCoreProtocol specifications."
+    description = "Generate Python Protobuf modules from protobuf files specifications."
     user_options = [
-        # TODO: put the options like --proto_path and --python_out here
+        ("--proto_path", None, "Path to OEFCoreProtocol folder..")
     ]
 
     def run(self):
@@ -37,22 +36,15 @@ class ProtocCommand(distutils.cmd.Command):
 
     def initialize_options(self):
         """Set default values for options."""
-        # Each user option must be listed here with their default value.
-        # TODO: complete
-        pass
+        self.proto_path = "OEFCoreProtocol"
 
     def finalize_options(self):
         """Post-process options."""
-        # TODO complete
-        pass
+        assert os.path.exists(self.proto_path), (
+                'Directory %s does not exist.' % self.proto_path)
 
     def _find_protoc_executable_path(self):
-        # TODO: fix (and test) how to find protoc executable for other OS
-        # the next line works in Python 3 but not in Python 2.
-        # result = shutil.which("protoc")
-
-        # Works also in Python 2
-        result = os.popen("which protoc").read().strip()
+        result = shutil.which("protoc")
 
         if result is None or result == "":
             raise EnvironmentError("protoc compiler not found.")
@@ -63,17 +55,15 @@ class ProtocCommand(distutils.cmd.Command):
         command = [protoc_executable_path] + self._get_arguments()
         return command
 
-    # TODO: generalize to other system path pattern (e.g. Windows)
     def _get_arguments(self):
         arguments = []
-        arguments.append("--proto_path=./OEFCoreProtocol")
-        arguments.append("--python_out=./oef")
-        arguments += glob.glob("OEFCoreProtocol/*.proto") # TODO add recursive search
+        arguments.append("--proto_path=%s" % self.proto_path)
+        arguments.append("--python_out=oef")
+        arguments += glob.glob(os.path.join("OEFCoreProtocol", "*.proto"))
         return arguments
 
     def _fix_import_statements_in_all_protobuf_modules(self):
-        # TODO: generalize to other system path pattern (e.g. Windows' one)
-        generated_protobuf_python_modules = glob.glob("oef/*_pb2.py")
+        generated_protobuf_python_modules = glob.glob(os.path.join("oef", "*_pb2.py"))
         for filepath in generated_protobuf_python_modules:
             self._fix_import_statements_in_protobuf_module(filepath)
 

e0b5c17bb0b7eb93d43226376f38e62f52c69e98
Marco Favorito <marco.favorito@gmail.com>
2018-12-16 23:28:58 +0000

    Add more docstrings.

--- oef/proxy.py
+++ oef/proxy.py
@@ -226,9 +226,12 @@ class OEFLocalProxy(OEFProxy):
     """
 
     class LocalNode:
-        """A light-weight implementation of a OEF Node."""
+        """A light-weight local implementation of a OEF Node."""
 
         def __init__(self):
+            """
+            Initialize a local (i.e. non-networked) implementation of an OEF Node
+            """
             self.agents = dict()                     # type: Dict[str, Description]
             self.services = defaultdict(lambda: [])  # type: Dict[str, List[Description]]
             self._lock = asyncio.Lock()
@@ -239,11 +242,22 @@ class OEFLocalProxy(OEFProxy):
             self.loop = asyncio.get_event_loop()
 
         def connect(self, public_key: str) -> asyncio.Queue:
+            """
+            Connect a public key to the node.
+
+            :param public_key: the public key of the agent.
+            :return: an asynchronous queue, that constitutes the communication channel.
+            """
             queue = asyncio.Queue()
             self.queues[public_key] = queue
             return queue
 
-        async def _process_messages(self):
+        async def _process_messages(self) -> None:
+            """
+            Main event loop to process the incoming messages.
+
+            :return: ``None``
+            """
             while True:
                 try:
                     data = await self.read_queue.get()  # type: Tuple[str, BaseMessage]
@@ -255,30 +269,66 @@ class OEFLocalProxy(OEFProxy):
                 assert isinstance(msg, AgentMessage)
                 self._send_agent_message(public_key, msg)
 
-        async def run(self):
+        async def run(self) -> None:
+            """
+            Run the node, i.e. start processing the messages.
+
+            :return: ``None``
+            """
             self._task = asyncio.ensure_future(self._process_messages())
             await self._task
 
-        def stop(self):
+        def stop(self) -> None:
+            """
+            Stop the execution of the node.
+
+            :return: ``None``
+            """
             if self._task:
                 self._task.cancel()
 
         def register_agent(self, public_key: str, agent_description: Description) -> None:
+            """
+            Register an agent in the agent directory of the node.
+
+            :param public_key: the public key of the agent to be registered.
+            :param agent_description: the description of the agent to be registered.
+            :return: ``None``
+            """
             self.loop.run_until_complete(self._lock.acquire())
             self.agents[public_key] = agent_description
             self._lock.release()
 
         def register_service(self, public_key: str, service_description: Description):
+            """
+            Register a service agent in the service directory of the node.
+
+            :param public_key: the public key of the service agent to be registered.
+            :param service_description: the description of the service agent to be registered.
+            :return: ``None``
+            """
             self.loop.run_until_complete(self._lock.acquire())
             self.services[public_key].append(service_description)
             self._lock.release()
 
         def unregister_agent(self, public_key: str) -> None:
+            """
+            Unregister an agent.
+
+            :param public_key: the public key of the agent to be unregistered.
+            :return: ``None``
+            """
             self.loop.run_until_complete(self._lock.acquire())
             self.agents.pop(public_key)
             self._lock.release()
 
         def unregister_service(self, public_key: str, service_description: Description) -> None:
+            """
+            Unregister a service agent.
+
+            :param public_key: the public key of the service agent to be unregistered.
+            :return: ``None``
+            """
             self.loop.run_until_complete(self._lock.acquire())
             self.services[public_key].remove(service_description)
             if len(self.services[public_key]) == 0:
@@ -296,6 +346,13 @@ class OEFLocalProxy(OEFProxy):
             self._send_search_result(public_key, search_id, sorted(self.services.keys()))
 
         def _send_agent_message(self, origin: str, msg: AgentMessage) -> None:
+            """
+            Send an :class:`~oef.messages.AgentMessage`.
+
+            :param origin: the public key of the sender agent.
+            :param msg: the message.
+            :return: ``None``
+            """
             e = msg.to_envelope()
             destination = e.send_message.destination
 
@@ -312,6 +369,14 @@ class OEFLocalProxy(OEFProxy):
             self.queues[destination].put_nowait(new_msg.SerializeToString())
 
         def _send_search_result(self, public_key: str, search_id: int, agents: List[str]) -> None:
+            """
+            Send a search result.
+
+            :param public_key: the public key of the agent to whom to send the search result.
+            :param search_id: the id of the search request.
+            :param agents: the list of public key of the agents/services to be returned.
+            :return:
+            """
             msg = agent_pb2.Server.AgentMessage()
             msg.agents.search_id = search_id
             msg.agents.agents.extend(agents)
